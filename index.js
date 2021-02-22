2bc93ad66ece92968c12157fe98ddc63d8634549
Nick Stanish <nickstanish@gmail.com>
2021-02-21 20:22:34 -0600

    Fix heroku app start

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const fastify = require('fastify')({
   logger: true
 });
-const { PORT } = require('./lib/constants');
+const { BIND_ADDRESS, PORT } = require('./lib/constants');
+const { v4: uuidv4 } = require('uuid');
+
+const PROCESS_GID = uuidv4();
 
 fastify.get('/_health', async (request, reply) => {
   return 'OK';
@@ -13,9 +16,14 @@ fastify.get('/_health.json', async (request, reply) => {
   };
 });
 
+fastify.setNotFoundHandler(async (request, reply) => {
+  return reply.status(404).send('Not Found');
+});
+
 const start = async () => {
   try {
-    await fastify.listen(PORT);
+    fastify.log.info(`Starting server ${PROCESS_GID}, WEB_CONCURRENCY=${process.env.WEB_CONCURRENCY}`);
+    await fastify.listen(PORT, BIND_ADDRESS);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
